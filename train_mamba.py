0d1c3a0d3cf7202bb665018b5fd2f64ce02348eb
Manuel Romero <mrm8488@gmail.com>
2024-01-23 20:30:07 +0000

    Set batch_size

--- train_mamba.py
+++ train_mamba.py
@@ -44,6 +44,7 @@ def run(args):
             learning_rate=args.learning_rate,
             num_train_epochs=args.num_epochs,
             per_device_train_batch_size=args.batch_size,
+            per_device_eval_batch_size=args.batch_size,
             gradient_accumulation_steps=args.gradient_accumulation_steps,
             optim=args.optim,
             output_dir=args.output_dir,
